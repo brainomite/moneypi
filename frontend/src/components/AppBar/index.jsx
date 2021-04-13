013fe88c26a227c16eff7b8de185d01195d68756
Aaron Young <hi@aaronyoung.dev>
2021-04-13 16:47:27 -0400

    bug: Add saved index.jsx

--- frontend/src/components/AppBar/index.jsx
+++ frontend/src/components/AppBar/index.jsx
@@ -1,6 +1,5 @@
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
-import GithubCorner from "react-github-corner";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import React from "react";
