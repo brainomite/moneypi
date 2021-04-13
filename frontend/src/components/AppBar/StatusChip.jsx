bf62812ea45d23164fc36dc1a1fbbbc6c97ee9e8
Aaron Young <hi@aaronyoung.dev>
2021-04-13 18:58:54 -0400

    feat: Add local detect feature

--- frontend/src/components/AppBar/StatusChip.jsx
+++ frontend/src/components/AppBar/StatusChip.jsx
@@ -1,14 +1,9 @@
 import { makeStyles } from "@material-ui/core/styles";
 import Chip from "@material-ui/core/Chip";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import RefreshIcon from "@material-ui/icons/Refresh";
 import SentimentDissatisfiedIcon from "@material-ui/icons/SentimentDissatisfied";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import Button from "@material-ui/core/Button";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   root: { marginRight: theme.spacing(3) },
@@ -16,46 +11,29 @@ const useStyles = makeStyles((theme) => ({
 
 export default function StatusChip() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [status, setStatus] = useState("loading");
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+  useEffect(() => {
+    if (status !== "loading") return;
+    const hostname =
+      process.env.NODE_ENV === "production" ? "moneypi.local" : "localhost";
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    axios
+      .get(`http://${hostname}:8080`)
+      .then(() => setStatus("connected"))
+      .catch(() => setStatus("disconnected"));
+  }, [status]);
+
+  const handleRefresh = () => setStatus("loading");
+
   return (
-    <>
-      <Chip
-        icon={<SentimentDissatisfiedIcon />}
-        label="offline"
-        clickable
-        onDelete={handleClickOpen}
-        onClick={handleClickOpen}
-        deleteIcon={<RefreshIcon />}
-        variant="outlined"
-        className={classes.root}
-      />
-      <Dialog
-        open={open}
-        keepMounted
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-slide-title"
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle id="alert-dialog-slide-title">Not Implemented</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            This is a placeholder for a future feature, thank you for clicking!
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Understood
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
+    <Chip
+      icon={<SentimentDissatisfiedIcon />}
+      label={status}
+      onDelete={handleRefresh}
+      deleteIcon={<RefreshIcon />}
+      variant="outlined"
+      className={classes.root}
+    />
   );
 }
